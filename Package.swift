69da245c6f50b45d7a1d8c21a66ae43c52dcfb49
Laith K <l.kaldyne@gmail.com>
2022-07-19 17:09:12 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
     name: "DateToolsSwift",
     targets: [
         Target(name: "DateToolsSwift")
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
 package.exclude = ["DateTools", "Examples", "Tests", "DateToolsSwift/Examples"]
